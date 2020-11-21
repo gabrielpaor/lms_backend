008977de50a4c538ced99bde87ad6d16813fd182
ikazreal21 <expliot@gmail.com>
2020-11-21 18:05:23 +0800

    🎨 static updates,views,urls

--- student_backend/views.py
+++ student_backend/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def dashboard(request):
+    return render(request)
+ 
+
+def profile(request):
+    return render(request)
+
+
+def groups(request):
+    return render(request)
+
+
+def subject(request):
+    return render(request)
+
+def calendar(request):
+    return render(request)
+
+def grades(request):
+    return render(request)
+
+
+def settings(request):
+    return render(request)
